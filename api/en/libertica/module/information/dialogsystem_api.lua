3a8c2004016b10336b2cfbf552810ea645c58049
totalwarANGEL <n3crosh8d3@gmail.com>
2025-05-30 00:32:08 +0200

    Simplify information module doc

--- api/en/libertica/module/information/dialogsystem_api.lua
+++ api/en/libertica/module/information/dialogsystem_api.lua
@@ -1,25 +1,23 @@
---- Allows to define dialogs
+--- Allows defining dialogs.
 ---
---- Dialogs can be used to create conversations between characters using the
---- animated heads in a feature stripped briefing.
+--- Dialogs can be used to create conversations between characters using
+--- animated heads in a function-stripped briefing.
 ---
-
-
 
 --- Starts a dialog.
 ---
---- Possible fields for the dialog table:
---- * `Starting`                - Function called when dialog is started              
---- * `Finished`                - Function called when dialog is finished             
---- * `RestoreCamera`           - Camera position is saved and restored at dialog end 
---- * `RestoreGameSpeed`        - Game speed is saved and restored at dialog end      
---- * `EnableGlobalImmortality` - During dialogs all entities are invulnerable        
---- * `EnableSky`               - Display the sky during the dialog                   
---- * `EnableFoW`               - Displays the fog of war during the dialog           
---- * `EnableBorderPins`        - Displays the border pins during the dialog
---- * `HideNotes`               - Do not show notes
+--- The dialog itself can have various attributes.
+--- * `Starting`                - Function called when the dialog starts              
+--- * `Finished`                - Function called when the dialog ends             
+--- * `RestoreCamera`           - Camera position is saved and restored at the end of the dialog 
+--- * `RestoreGameSpeed`        - Game speed is saved and restored at the end of the dialog      
+--- * `EnableGlobalImmortality` - All units are invulnerable during the dialog       
+--- * `EnableSky`               - Show sky during the dialog                  
+--- * `EnableFoW`               - Show fog of war during the dialog          
+--- * `EnableBorderPins`        - Show border pins during the dialog
+--- * `HideNotes`               - Do not display messages
 ---
---- #### Example:
+--- #### Example
 ---
 --- ```lua
 --- function Dialog1(_Name, _PlayerID)
@@ -28,9 +26,7 @@
 ---         DisableBoderPins = true,
 ---     };
 ---     local AP, ASP = API.AddDialogPages(Dialog);
----
----     -- Pages ...
----
+---     -- Pages
 ---     Dialog.Starting = function(_Data)
 ---     end
 ---     Dialog.Finished = function(_Data)
@@ -40,23 +36,23 @@
 --- ```
 ---
 --- @param _Dialog table     Dialog table
---- @param _Name string      Name of dialog
---- @param _PlayerID integer Player ID of receiver
+--- @param _Name string      Name of the dialog
+--- @param _PlayerID integer Player ID of the recipient
 function StartDialog(_Dialog, _Name, _PlayerID)
 end
 API.StartDialog = StartDialog;
 
---- Asks the player for the permission to change graphic settings.
+--- Asks the player for permission to change graphics settings.
 ---
---- If the BriefingSystem is loaded, it's functionality will be used.
+--- If the BriefingSystem is loaded, its functionality is used.
 ---
---- This feature is deactivated in Multiplayer.
+--- This functionality is disabled in multiplayer.
 function RequestDialogAlternateGraphics()
 end
 API.RequestDialogAlternateGraphics = RequestDialogAlternateGraphics;
 
---- Checks if a dialog ist active.
---- @param _PlayerID integer PlayerID of receiver
+--- Checks if a dialog is active.
+--- @param _PlayerID integer Player ID of the recipient
 --- @return boolean IsActive Dialog is active
 function IsDialogActive(_PlayerID)
     return true;
@@ -65,10 +61,10 @@ API.IsDialogActive = IsDialogActive;
 
 --- Prepares the dialog and returns the page functions.
 ---
---- Must be called before pages are added.
+--- Must be called before adding pages.
 --- @param _Dialog table Dialog table
 --- @return function AP  Page function
---- @return function ASP Short page function
+--- @return function ASP Simple page function
 function AddDialogPages(_Dialog)
     return function(...) end, function(...) end
 end
@@ -76,167 +72,111 @@ API.AddDialogPages = AddDialogPages;
 
 --- Creates a page.
 ---
---- #### Dialog Page
---- Possible fields for the page:
+--- The page can have various attributes.
+--- * `Actor`      - (optional) Player ID of the speaker
+--- * `Title`      - (optional) Name of the actor (only with Actor)
+--- * `Text`       - (optional) Displayed page text
+--- * `Speech`     - Path to the voiceover (MP3 file)
+--- * `Position`   - Camera position (not with Target)
+--- * `Target`     - Entity the camera follows (not with Position)
+--- * `Distance`   - (optional) Camera distance
+--- * `Action`     - (optional) Function called when the page is shown
+--- * `FadeIn`     - (optional) Duration of fade-in from black
+--- * `FadeOut`    - (optional) Duration of fade-out to black
+--- * `FaderAlpha` - (optional) Mask alpha
+--- * `MC`         - (optional) Table with options for branching dialogs
 ---
---- * `Actor`           - (optional) PlayerID of speaker
---- * `Title`           - (optional) Name of actor (only with actor)
---- * `Text`            - (optional) Displayed page text
---- * `Speech`          - Path to voiceover (MP3 file)
---- * `Position`        - Position of camera (not with target)
---- * `Target`          - Entity the camera follows (not with position)
---- * `Distance`        - (optional) Distance of camera
---- * `Action`          - (optional) Function called when page is displayed
---- * `FadeIn`          - (optional) Duration of fadein from black
---- * `FadeOut`         - (optional) Duration of fadeout to black
---- * `FaderAlpha`      - (optional) Mask alpha
---- * `MC`              - (optional) Table with choices to branch of in dialogs
----
---- *→ Example #1*
----
---- #### Flow control
---- In a dialog the player can be forced to make a choice that will have
---- different results. That is called multiple choice. Options must be provided
---- in a table. The target page can be defined with it's name or a function can
---- be provided for more control over the flow. Such funktions must return a
---- page name.
----
---- *→ Example #2*
----
---- Additionally each function can be marked to be removed when used
---- and not shown again when reentering the page.
----
---- *→ Example #3*
----
---- Also pages can be hidden by providing a function to check conditions.
----
---- *→ Example #4*
----
---- If a dialog is branched it must be manually ended after a branch is done
---- or it just simply shows the next page. To end a dialog, an empty page
---- must be added.
----
---- *→ Example #5*
----
---- Alternativly the dialog can continue at a different page. This allows to
---- create repeating structures within a dialog.
----
---- *→ Example #6*
----
---- To obtain selected answers at a later point the selection can be saved in a
---- global variable either in a option callback or in the finished function. The
---- number returned is the ID of the answer.
----
---- *→ Example #7*
----
---- #### Examples
----
---- * Example #1: A simple page
+--- #### Example:
+--- Create a simple page.
 --- ```lua
 --- AP {
----     Title        = "Hero",
----     Text         = "This page has an actor and a choice.",
----     Actor        = 1,
----     Duration     = 2,
----     FadeIn       = 2,
----     Position     = "npc1",
----     DialogCamera = true,
+---    Title        = "Marcus",
+---    Text         = "This is a simple page.",
+---    Actor        = 1,
+---    Duration     = 2,
+---    FadeIn       = 2,
+---    Position     = "npc1",
+---    DialogCamera = true,
 --- };
 --- ```
 ---
----
---- * Example #2: Usage of multiple choices
+--- #### Example:
+--- Create a multiple choice page.
 --- ```lua
--- AP {
----     Title        = "Hero",
----     Text         = "This page has an actor and a choice.",
----     Actor        = 1,
----     Duration     = 2,
----     FadeIn       = 2,
----     Position     = "npc1",
----     DialogCamera = true,
----     MC = {
----         {"Option 1", "TargetPage"},
----         {"Option 2", Option2Clicked},
----     },
+--- AP {
+---    Title        = "Marcus",
+---    Text         = "This is not such a simple page.",
+---    Actor        = 1,
+---    Duration     = 2,
+---    FadeIn       = 2,
+---    Position     = "npc1",
+---    DialogCamera = true,
+---    MC = {
+---        {"Option 1", "Option1"},
+---        {"Option 2", "Option2"},
+---    },
 --- };
+--- 
+--- -- The branches in a briefing must be separated by an empty page
+--- -- so the briefing knows it's done here.
+--- ASP("Option1", "First Option", "This is the first option.", false, "Marcus");
+--- AP();
+--- ASP("Option2", "Second Option", "This is the second option.", false, "Marcus");
 --- ```
 ---
----
---- * Example #3: One time usage option
+--- #### Example:
+--- The jump target of an option can be determined by a function.
 --- ```lua
---- MC = {
----     ...,
----     {"Option 3", "AnotherPage", Remove = true},
---- }
---- ```
----
----
---- * Example #4: Option with condition
---- ```lua
---- MC = {
----     ...,
----     {"Option 3", "AnotherPage", Disable = OptionIsDisabled},
---- }
---- ```
----
----
---- * Example #5: Abort dialog
---- ```lua
---- AP()
---- ```
----
----
---- * Example #6: Jump to other page
---- ```lua
---- AP("SomePageName")
---- ```
----
----
---- * Example #7: Get selected option
---- ```lua
---- Dialog.Finished = function(_Data)
----     MyChoosenOption = _Data:GetPage("Choice"):GetSelected();
---- end
+--- AP {
+---    Title        = "Marcus",
+---    Text         = "This is not such a simple page.",
+---    Actor        = 1,
+---    Duration     = 2,
+---    FadeIn       = 2,
+---    Position     = "npc1",
+---    DialogCamera = true,
+---    MC = {
+---        {"Option 1", "Option1"},
+---        {"Option 2", ForkingFunction},
+---    },
+--- };
 --- ```
 ---
 function AP(_Data)
-    assert(false);
 end
 
---- Creates a page in a simplified manner.
+--- Creates a page in a simplified way.
 ---
---- The function can create a automatic page name based of the page index. A
---- name can be an optional parameter at the start.
+--- The function can automatically generate a page name based on the page index.
+--- A name can be optionally provided as the first parameter.
 ---
 --- #### Settings
 --- The function expects the following parameters:
 --- 
---- * `Name`           - (Optional) Name of page
---- * `Sender`         - Player ID of actor
---- * `Target`         - Entity the camera is looking at
---- * `Title`          - Displayed page title
---- * `Text`           - Displayed page text
---- * `DialogCamera`   - Use closeup camera
---- * `Action`         - (Optional) Action when page is shown
+--- * `Name`         - (Optional) Name of the page
+--- * `Sender`       - Player ID of the actor
+--- * `Target`       - Entity the camera focuses on
+--- * `Title`        - Displayed page title
+--- * `Text`         - Displayed page text
+--- * `DialogCamera` - Use dialog close-up camera
+--- * `Action`       - (Optional) Action when the page is shown
 ---
---- #### Examples
+--- #### Example:
 ---
 --- ```lua
---- -- Long shot
+--- -- Wide shot
 --- ASP("Title", "Some important text.", false, "HQ");
---- -- Page Name
+--- -- With page name
 --- ASP("Page1", "Title", "Some important text.", false, "HQ");
---- -- Close-Up
+--- -- Close-up
 --- ASP("Title", "Some important text.", true, "Marcus");
---- -- Call action
+--- -- Triggering an action
 --- ASP("Title", "Some important text.", true, "Marcus", MyFunction);
---- -- Allow/forbid skipping
+--- -- Allowing/disallowing skip
 --- ASP("Title", "Some important text.", true, "HQ", nil, true);
 --- ```
 ---
---- @param ... any List of page parameters
+--- @param ... any List of page settings
 function ASP(...)
-    assert(false);
 end
 
