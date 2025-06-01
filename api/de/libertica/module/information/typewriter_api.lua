1349938e247f55fcf0b7f8afb3373d026ece9095
totalwarANGEL <n3crosh8d3@gmail.com>
2025-06-01 12:48:23 +0200

    Fix doc format

--- api/de/libertica/module/information/typewriter_api.lua
+++ api/de/libertica/module/information/typewriter_api.lua
@@ -1,31 +1,33 @@
---- ...
----
-Lib.Typewriter = Lib.Typewriter or {};
+--- Dieses Modul ermöglicht die Anzeige eines Textes im Schreibmaschienstil.
+--- 
+--- #### Funktionsweise
+--- Der Text wir Zeichenweise auf dem Bildschirm ausgegeben. Dabei wird der
+--- Text in Token zerlegt. Normale Zeichen ergeben jeweils ein Token. Wird
+--- eine spezielle Zeichenkette (z.B. {cr}) gefunden, wird diese als ein 
+--- Token behandelt. Mehrere Leerzeichen hindereinander werden (innerhalb
+--- der Engine) zu einem Leerzeichen verkürzt.
+--- 
+--- Wenn bei Spielstart verwendet, beginnt der Text nach dem Laden der Karte.
+--- Wenn ein anderes Kino-Event läuft, wartet der Typewriter auf den Abschluss.
 
 
 
---- Zeigt einen Text byte für byte an.
+--- Zeigt einen Text Zeichen für Zeichen an.
 ---
---- Wenn bei Spielstart verwendet, beginnt der Text nach dem Laden der Karte. Wenn ein anderes
---- Kinoevent läuft, wartet der Typewriter auf Abschluss.
 ---
---- Kontrollzeichen wie {cr} werden als ein Token ausgewertet und als atomares Token behandelt
---- und sofort angezeigt. Mehr als 1 Leerzeichen hintereinander werden automatisch auf 1 Leerzeichen
---- abgeschnitten (durch den Spiel-Engine).
+--- #### Fields `_Data`:
+--- * `Text`:         <b>string</b> Anzuzeigender Text
+--- * `Name`:         (Optional) <b>string</b> Name für das Ereignis
+--- * `PlayerID`:     (Optional) <b>integer</b> Spieler, dessen Text angezeigt wird
+--- * `Callback`:     (Optional) <b>function</b> Callback Function
+--- * `TargetEntity`: (Optional) <b>string</b> Entität, auf die die Kamera fokussiert ist
+--- * `CharSpeed`:    (Optional) <b>integer</b> Faktor der Schreibgeschwindigkeit (Standard: 1.0)
+--- * `Waittime`:     (Optional) <b>integer</b> Anfangswartezeit vor dem Schreiben
+--- * `Opacity`:      (Optional) <b>float</b> Deckkraft des Hintergrunds (Standard: 1.0)
+--- * `Color`:        (Optional) <b>table</b> Hintergrundfarbe (Standard: {R= 0, G= 0, B= 0})
+--- * `Image`:        (Optional) <b>string</b> Hintergrundbild (muss im 16:9-Format sein)
 ---
---- #### Felder der Tabelle
---- * Text         - Anzuzeigender Text
---- * Name         - (Optional) Name für das Ereignis
---- * PlayerID     - (Optional) Spieler, dessen Text angezeigt wird
---- * Callback     - (Optional) Rückruffunktion
---- * TargetEntity - (Optional) Entität, auf die die Kamera fokussiert ist
---- * CharSpeed    - (Optional) Faktor der Schreibgeschwindigkeit (Standard: 1.0)
---- * Waittime     - (Optional) Anfangswartezeit vor dem Schreiben
---- * Opacity      - (Optional) Deckkraft des Hintergrunds (Standard: 1.0)
---- * Color        - (Optional) Hintergrundfarbe (Standard: {R= 0, G= 0, B= 0})
---- * Image        - (Optional) Hintergrundbild (muss im 16:9-Format sein)
----
---- #### Beispiele
+--- #### Example:
 --- ```lua
 --- local EventName = StartTypewriter {
 ---     PlayerID = 1,
